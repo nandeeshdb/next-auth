00b4d416d245f8fa4412bd34a91498202c330da8
nandeeshdb <nandeeshbasavarajaiah@gmail.com>
2024-03-19 23:17:08 +0530

    ReactHook Form

--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -9,6 +9,7 @@ import {
 } from "@heroicons/react/16/solid";
 import { Button, Checkbox, Input } from "@nextui-org/react";
 import React, { useState } from "react";
+import { useForm ,SubmitHandler, Controller} from "react-hook-form";
 import validator from "validator";
 import { z } from "zod";
 
@@ -18,12 +19,12 @@ const formSchema = z
       .string()
       .min(2, "Atleast 2 cahracters required")
       .max(50, "Only 50characters are allowed")
-      .regex(new RegExp("^[a-zA-Z]$", "Special Characters are not allowed")),
+      .regex(new RegExp("^[a-zA-Z]$"), "Special Characters are not allowed"),
     lastName: z
       .string()
       .min(2, "Atleast 2 cahracters required")
       .max(50, "Only 50characters are allowed")
-      .regex(new RegExp("^[a-zA-Z]$", "Special Characters are not allowed")),
+      .regex(new RegExp("^[a-zA-Z]$"), "Special Characters are not allowed"),
     email: z.string().email("Please enter correct email address"),
     phoneNumber: z
       .string()
@@ -43,31 +44,44 @@ const formSchema = z
     path: ["password", "confirmPassword"],
   });
 
+
+  type inputType = z.infer<typeof formSchema>
+
 function SignUpForm() {
+  const{register,handleSubmit,reset,control} = useForm<inputType>()
   const [showPassword, setShowPassword] = useState(false);
   const showPasswordHandler = () => {
     setShowPassword((prev) => !prev);
   };
+
+  const saveUser : SubmitHandler<inputType>=async(data)=>{
+    console.log({data})
+  }
   return (
-    <form className="flex flex-col w-full px-96 gap-3">
+    <form className="flex flex-col w-full px-96 gap-3" onSubmit={handleSubmit(saveUser)}>
       <Input
         label="First Name"
+        {...register("firstName")}
         startContent={<UserCircleIcon className="w-4 h-4" />}
       />
       <Input
+      {...register("lastName")}
         label="Last Name"
         startContent={<UserCircleIcon className="w-4 h-4" />}
       />
       <Input
+      {...register("phoneNumber")}
         label="Phone number"
         startContent={<PhoneIcon className="w-4 h-4" />}
       />
       <Input
+      {...register("email")}
         label="Email"
         startContent={<EnvelopeIcon className="w-4 h-4" />}
         type="email"
       />
       <Input
+      {...register("password")}
         label="Password"
         startContent={<KeyIcon className="w-4 h-4" />}
         type={showPassword ? "text" : "password"}
@@ -86,12 +100,17 @@ function SignUpForm() {
         }
       />
       <Input
+      {...register("confirmPassword")}
         label="Confirm Password"
         startContent={<KeyIcon className="w-4 h-4" />}
         type={showPassword ? "text" : "password"}
       />
 
-      <Checkbox>I accept the terms and condition</Checkbox>
+      
+      <Controller control={control} name="accepted" render={({field})=>(
+
+        <Checkbox onChange={field.onChange} onBlur={field.onBlur}>I accept the terms and condition</Checkbox>
+      )} />
 
       <Button type="submit" color="primary">
         Sign Up
